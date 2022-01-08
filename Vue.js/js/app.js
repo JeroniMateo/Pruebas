cca85801afe42eebbd61bec9026e68b3eae32bbd
Jeroni <jmateo@cifpfbmoll.eu>
2022-01-08 12:03:06 +0100

    7. Componentes

--- Vue.js/js/app.js
+++ Vue.js/js/app.js
@@ -1,48 +1,64 @@
+Vue.component("user", {
+  props: ["name", "lastName"],
+  data: function () {
+    return {
+      app: {
+        name: "Aprendible",
+      },
+    };
+  },
+  template: `<div>
+    <h1> Usuario de  {{ app.name }} </h1> 
+    <h2> Nombre: {{ name }} {{lastName}} </h2>
+    <input v-model="name" >
+    <input v-model="app.name" >
+  </div>`,
+});
+
 var app = new Vue({
-    el: "#app",
-    data: {
-      newTask: "",
-      tasks: [
-        { title: "Aprendible PHP", completed: true },
-        { title: "Aprendible Laravel", completed: false },
-        { title: "Aprendible VueJS", completed: true },
-      ],
+  el: "#app",
+  data: {
+    newTask: "",
+    tasks: [
+      { title: "Aprendible PHP", completed: true },
+      { title: "Aprendible Laravel", completed: false },
+      { title: "Aprendible VueJS", completed: true },
+    ],
+  },
+  methods: {
+    addTask: function () {
+      if (this.newTask.length <= 1) return alert("Tarea vacia");
+
+      //Con el metodo Push añadimos un nuevo elemento
+      this.tasks.push({
+        title: this.newTask,
+        completed: false,
+      });
+      this.newTask = "";
     },
-    methods: {
 
-
-
-      addTask: function () {
-        if (this.newTask.length <= 1) return alert("Tarea vacia");
-
-        //Con el metodo Push añadimos un nuevo elemento
-        this.tasks.push({
-          title: this.newTask,
-          completed: false,
-        });
-        this.newTask = "";
-      },
-
-
-      taskClasses: function (task){
-        console.log('css changed')
-        return ['glyphicon' , task.completed ? 'glyphicon-check' : 'glyphicon-unchecked']
-      },
-
-      completeTask: function(task){
-        task.completed =  ! task.completed
-      }
+    taskClasses: function (task) {
+      console.log("css changed");
+      return [
+        "glyphicon",
+        task.completed ? "glyphicon-check" : "glyphicon-unchecked",
+      ];
     },
-    computed: {
-      completedTasks: function () {
-        return this.tasks.filter(function (task) {
-          return task.completed;
-        }).length;
-      },
-      incompletedTasks: function () {
-        return this.tasks.filter(function (task) {
-          return !task.completed;
-        }).length;
-      },
+
+    completeTask: function (task) {
+      task.completed = !task.completed;
     },
-  });
+  },
+  computed: {
+    completedTasks: function () {
+      return this.tasks.filter(function (task) {
+        return task.completed;
+      }).length;
+    },
+    incompletedTasks: function () {
+      return this.tasks.filter(function (task) {
+        return !task.completed;
+      }).length;
+    },
+  },
+});
