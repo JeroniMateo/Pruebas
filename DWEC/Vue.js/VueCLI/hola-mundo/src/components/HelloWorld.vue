7b7762d665884fb6d0ae0ca0d7abef23bc974ba0
Jeroni <jmateo@cifpfbmoll.eu>
2022-02-05 11:43:39 +0100

    prova cli vue

--- DWEC/Vue.js/VueCLI/hola-mundo/src/components/HelloWorld.vue
+++ DWEC/Vue.js/VueCLI/hola-mundo/src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2> {{ salutacio }} </h2>
+    
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,7 +37,12 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+    salutacio: "Benvinguts",
   }
+  },
 }
 </script>
 
@@ -54,5 +61,8 @@ li {
 }
 a {
   color: #42b983;
+}
+h2{
+  color: red;
 }
 </style>
